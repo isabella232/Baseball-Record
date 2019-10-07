f7fe4eeb4004159385bdda0fd873ca9f2f509cbf
jhvoong <jhvoong@ucdavis.edu>
2019-10-07 13:00:42 -0700

    Changed variable names from lpa to shf

--- Baseball Record.py
+++ Baseball Record.py
@@ -1,13 +1,13 @@
 def checkRecord(n):
-    """ O(n) 
+    """ O(n) time with O(1) space solution
     """
     
-    lp = [1 for _ in range(n+1)]
-    Pp, Lp, LLp  = 1, 0, 0
+    sh = [1 for _ in range(n+1)]
+    Hp, Sp, SSp  = 1, 0, 0
     for i in range(1, n+1):
-        LLp = Lp
-        Lp  = Pp
-        Pp  = lp[i-1]
-        lp[i] = (LLp + Lp + Pp) % 1000000007 
+        SSp = Sp
+        Sp  = Hp
+        Hp  = sp[i-1]
+        sp[i] = (SSp + Sp + Hp) % 1000000007 
     
-    return (lp[n] + sum(((lp[i] * lp[n-i-1]) % 1000000007 for i in range(n)))) % 1000000007		
+    return (sp[n] + sum(((sp[i] * sp[n-i-1]) % 1000000007 for i in range(n)))) % 1000000007		
